1bb6c43b04dd2cab564dc188bfae1768fb014dab
asmam282891-crypto <asmam282891@gmail.com>
2026-09-01 12:59:19 +0200

    Refactor AppTextField widget structure
    Refactor AppTextField to use TextFormField directly and add optional suffixIcon.

--- lib/widgets/app_text_field.dart
+++ lib/widgets/app_text_field.dart
@@ -1,50 +1,40 @@
 import 'package:flutter/material.dart';
-import '../theme/app_theme.dart';
 
 class AppTextField extends StatelessWidget {
   final String label;
   final TextEditingController controller;
-  final bool obscureText;
   final TextInputType keyboardType;
+  final bool obscureText;
   final IconData? icon;
+  final Widget? suffixIcon;
   final String? Function(String?)? validator;
 
   const AppTextField({
     super.key,
     required this.label,
     required this.controller,
-    this.obscureText = false,
     this.keyboardType = TextInputType.text,
+    this.obscureText = false,
     this.icon,
+    this.suffixIcon,
     this.validator,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          label,
-          style: Theme.of(context)
-              .textTheme
-              .bodyMedium
-              ?.copyWith(fontWeight: FontWeight.w600),
+    return TextFormField(
+      controller: controller,
+      keyboardType: keyboardType,
+      obscureText: obscureText,
+      validator: validator,
+      decoration: InputDecoration(
+        labelText: label,
+        prefixIcon: icon != null ? Icon(icon) : null,
+        suffixIcon: suffixIcon,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
         ),
-        const SizedBox(height: 8),
-        TextFormField(
-          controller: controller,
-          obscureText: obscureText,
-          keyboardType: keyboardType,
-          validator: validator,
-          textAlign: TextAlign.right,
-          decoration: InputDecoration(
-            suffixIcon: icon != null
-                ? Icon(icon, color: AppColors.textSecondary, size: 20)
-                : null,
-          ),
-        ),
-      ],
+      ),
     );
   }
 }
