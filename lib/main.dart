dc4e66abc6b7987d41c2df672ea454e7f8ab4b81
asmam282891-crypto <asmam282891@gmail.com>
2026-09-13 11:34:38 +0200

    Add import for employee attendance report screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:ambulance_attendance/screens/employee_attendance_report_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
